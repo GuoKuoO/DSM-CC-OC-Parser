735797611d4b0a8586710ad2ba1d2e87b3e19ba2
GuoKuoO <guoyaosong@gmail.com>
2014-09-03 19:44:19 +0800

    1. 创建常见数据类型定义及数据结构。

---
+++ include/common.h
@@ -0,0 +1,44 @@
+#ifndef __COMMON_H_
+#define __COMMON_H_
+
+/* 常用数据类型定义 */
+
+typedef signed char        int8_t;
+typedef unsigned char      uint8_t;
+typedef short              int16_t;
+typedef unsigned short     uint16_t;
+typedef int                int32_t;
+typedef unsigned int       uint32_t;
+
+
+/* 常用数据结构定义及接口 */
+
+typedef struct node_t
+{
+    void    *data;
+
+    struct node_t *next;
+    struct node_t *prev;
+}node_t;
+
+typedef struct list_t
+{
+    node_t   head;
+
+    int32_t  size;
+    int32_t  cont;
+}list_t;
+
+list_t *list_create(int32_t size);
+
+int32_t list_destroy(list_t *list);
+
+int32_t list_node_append(list_t *list, const void *data);
+
+int32_t list_node_remove(list_t *list, void *value); 
+
+void* list_node_obtain(list_t *list, int32_t positon);
+
+int32_t list_node_find(list_t *list, void *value);
+
+#endif
